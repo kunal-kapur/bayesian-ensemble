d5f5b8f28c69560dbfe1dd29c9c2f021777567da
kunal-kapur <kunalkapur888@gmail.com>
2025-03-09 16:57:50 -0400

    added experiments and analysis

--- test_hyperparms.py
+++ test_hyperparms.py
@@ -4,11 +4,15 @@ import torch
 import math
 
 # Define hyperparameter values
-dropout_hyperparms = [0.2, 0.4, 0.7]
+dropout_hyperparms = [0.7, 0.4]
 dropout_hyperparms = list(itertools.product(dropout_hyperparms, dropout_hyperparms))  # Pairs of dropout values
-num_masks_hyperparms = [3, 5, 10, 20]
-num_groups_hyperparms = [0.1, 0.3, 0.5]
-increment_amt_hyperparams = [1, 10, 50]
+# num_masks_hyperparms = [3, 5, 10, 20]
+num_masks_hyperparms = [1]
+
+num_groups_hyperparms = [0.1]
+increment_amt_hyperparams = [1]
+# num_groups_hyperparms = [0.1, 0.3, 0.5]
+# increment_amt_hyperparams = [1, 10, 50]
 
 # Get all possible combinations of hyperparameters
 hyperparameter_combinations = itertools.product(
@@ -22,7 +26,7 @@ seen = set()
 for dropout, num_masks, group_prop, increment_amt in hyperparameter_combinations:
     dropout1, dropout2 = dropout  # Unpacking the dropout tuple
     num_groups = math.ceil(group_prop / num_masks)
-    vals = (dropout1, dropout2, num_groups, increment_amt)
+    vals = (num_masks, dropout1, dropout2, num_groups, increment_amt)
     if vals in seen:
         continue
     seen.add(vals)
@@ -32,7 +36,6 @@ for dropout, num_masks, group_prop, increment_amt in hyperparameter_combinations
         "--dropout_probs", str(dropout1), str(dropout2),
         "--num_groups", str(num_groups),  # Convert proportion to integer
         "--increment_amt", str(increment_amt),
-
         "--path", PATH
     ]
     
